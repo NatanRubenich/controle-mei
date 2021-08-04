fe6cdefae7eca35668f0294a049ac6c53f35f28a
Natan Rubenich <57855715+NatanRubenich@users.noreply.github.com>
2021-08-04 00:09:37 -0400

    .

--- server/controllers/controllerTabela.js
+++
@@ -1,9 +0,0 @@
-import Usuario from '../models/usuario.js';
-import Tabela from '../models/tabela.js';
-
-
-export const getTabelaController = (req, res) => {
-  const resposta = res.locals.usuario;
-
-  return res.send({maisds: resposta , mais: "SIM"});
-};
